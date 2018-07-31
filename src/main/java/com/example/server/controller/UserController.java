460ea20e1503c96fcc89544e2664668a9e0be6b8
eugeneOwl <eugenevaleska1994@gmail.com>
2018-07-31 16:17:21 +0300

    CORS-filter class added to solve 403 error when deviding resources with front-end.

--- src/main/java/com/example/server/controller/UserController.java
+++ src/main/java/com/example/server/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.server.controller;
 import com.example.server.dto.UserDto;
 import com.example.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping("server/user")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     @Autowired
     private UserService userService;
