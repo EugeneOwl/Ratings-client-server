f867a2afc5a2ad7d4bf0701839ab5d98fb6c5d86
eugeneOwl <eugenevaleska1994@gmail.com>
2018-07-31 14:34:10 +0300

    UserController created. Sends userList JSON to URL.

---
+++ src/main/java/com/example/server/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.server.controller;
+
+import com.example.server.dto.UserDto;
+import com.example.server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("server/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public Collection<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
