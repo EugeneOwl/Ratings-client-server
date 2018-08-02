922c3b1db63b0e363d7881473b8514c44c0105a1
eugeneOwl <eugenevaleska1994@gmail.com>
2018-08-02 19:58:03 +0300

    Getting user by Id and Updating user implemented in User Controller.

--- src/main/java/com/example/server/controller/UserController.java
+++ src/main/java/com/example/server/controller/UserController.java
@@ -4,16 +4,14 @@ import com.example.server.dto.UserDto;
 import com.example.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "server/users", produces = MediaType.APPLICATION_JSON_VALUE)
-@CrossOrigin(origins = "http://127.0.1:4200")
+@CrossOrigin(origins = "http://127.0.0.1:4200")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -21,5 +19,21 @@ public class UserController {
     @GetMapping
     public List<UserDto> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("{id}")
+    public UserDto getById(@PathVariable("id") final int id) {
+
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public void updateUser(@RequestBody final UserDto userDto) {
+        if (Objects.isNull(userDto)) {
+
+            return;
+        }
+
+        this.userService.addOrUpdateUserIfValid(userDto);
     }
 }
